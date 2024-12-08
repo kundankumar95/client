f724df3b07228b48291cae91253a252facb813e4
Swarnendu <clickswarnendu123@gmail.com>
2024-12-08 13:22:19 +0530

    minor fixes

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,15 +1,16 @@
-import Navbar from './Components/Navbar/Navbar'
-import Admin from './Pages/Admin/Admin'
+import { Route, Routes } from "react-router-dom";
+import AddProduct from "./components/AddProduct/AddProduct";
+import ListProduct from "./components/ListProduct/ListProduct";
+import Home from "./pages/home.route";
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Admin/>
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/admin/product/add" element={<AddProduct />} />
+      <Route path="/admin/product/list" element={<ListProduct />} />
+    </Routes>
+  );
+};
 
-export default App
-
-
+export default App;
---
+++ admin/src/pages/home.route.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+     <a href="/admin/product/add">Add Product</a>
+        <a href="/admin/product/list">List Product</a>
+    </div>
+  );
+};
+
+export default Home;
